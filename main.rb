0a4afb2a361c561a7d8aa1fc4ef884f384a0c49c
Erka0007 <165401469+Erka0007@users.noreply.github.com>
2025-04-10 10:26:15 +0200

    test

--- main.rb
+++ main.rb
@@ -1,29 +1,46 @@
-def input()
-    puts "Hej! skriv ditt dagboksinlägg under"
-    text = gets.chomp
-    return text
-end
+
 
 def write(string, dag)
-    fil = File.open("dagbok//#{dag}.txt", "w")
+    if !File.exist?("dagbok//#{dag}.txt")
+        fil = File.open("dagbok//#{dag}.txt", "w")
+        fil.puts "Dag: #{dag}"
+        fil.puts "
+        "
+        fil.close
+    end
+    fil = File.open("dagbok//#{dag}.txt", "a")
     fil.puts string
     fil.close
 end
 
-def whichday()
+def read(dag)
+end
+
+def getDay()
     date = Time.now.strftime("%d-%m-%Y")
     return date
 end
 
-def whichtime()
+def getTime()
     time = Time.now.strftime("%H:%M")
     return time
 end
 
 def main()
-    write("hejehejehj", whichday())
-    write("hejehhj", 2)
-    p whichday()
+    run = true
+    i = 3
+    while run
+        puts "Hej! skriv ditt dagboksinlägg under eller om du vill avsluta programmet skriv 'esc'"
+        text = gets.chomp
+        if text == 'esc'
+            run = false
+            break
+        end
+
+        write(text,i)
+        
+        i += 1
+    end
 end
   
 main()
