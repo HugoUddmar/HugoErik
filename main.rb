fd634182a750f8cdf0f23f4f5d256f6bb80fc0bb
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2025-04-13 18:33:04 +0200

    fixade radbrytning

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 
-# Beskrivning: Den här funktionen tar en text och dagen och skapar en textfil med texten, och dagen blir namnet och rubriken i textfilen.
-# Om filen redan finns, kommer den endast lägga till texten.
+# Beskrivning: Den här funktionen tar en text och datum och skapar en textfil med texten, och dagen blir namnet och rubriken i textfilen.
+# Om filen redan finns, kommer den endast lägga till texten och fortsätta under det som redan skrivits.
 # 
 # Argument 1: String - Text som skrivs i textfilen
 # Argument 2: String - Datum som skrivs i textfilen som rubrik och titeln av textfilen
 # Return: Inget
-# Exempel: "Hej" => returnar ing
+# Exempel: "Hej" => returnar inget
 #
 #
 #
@@ -14,45 +14,51 @@ def write(string, dag)
     newString = ""
     newString = string
 
-    i = 49
-    while i < newString.length
-        if newString[i] == " "
-            newString[i] == "\n"
-        else
-            y = 0
-            while newString[i-y] != " "
-                y += 1
-            end
-            newString[i-y+1] = "\n"
-        end
-        i += 49
-    end
-
     i = 0
     while i < newString.length
         if newString[i] == "\t"
             newString[i] = "\n"
         end 
+        i += 1
+    end
 
+    count = 0
+    i = 0
+    while i < newString.length
+        if newString[i] == "\n"
+            count = 0
+        elsif count == 49
+            count = 0
+            if newString[i] == " "
+                newString[i] = "\n"
+            else
+                y = 0
+                while newString[i-y] != " "
+                    y += 1
+                end
+                newString[i-y] = "\n"
+            end
+        end
+        count += 1
         i += 1
     end
 
     if !File.exist?("dagbok//#{dag}.txt")
         fil = File.open("dagbok//#{dag}.txt", "w")
         fil.puts "Dag: #{dag}"
-        fil.puts "
-        "
+        fil.puts " "
         fil.close
     end
     fil = File.open("dagbok//#{dag}.txt", "a")
-    fil.puts string
+    fil.puts newString
     fil.close
 end
 
 def read(dag)
     x = Dir.chdir("dagbok")
     x = Dir.glob("*")
-    puts " vilken fil vill du läsa?"
+    puts " "
+    puts "Vilken fil vill du läsa?"
     i = 0
     while i < x.length
         puts "#{i + 1}: #{x[i]}"
@@ -79,7 +85,7 @@ end
 
 def main()
     run = true
-    i = 1
+    i = 5
     dagbokslista = []
     val = ["1","2","3"]
     while run
@@ -88,7 +94,7 @@ def main()
         1: skrivläge
         2: läsläge
         3: avsluta   
-      "
+        "
         choice = gets.chomp
        while !val.include?(choice)
         puts "fel input"
@@ -96,7 +102,7 @@ def main()
        end
 
         if choice == val[0]
-          puts "Skriv din text, om du vill byta rad, klicka på tab "
+          puts "Skriv din text, om du vill byta rad, klicka på tab"
           text = gets.chomp
           write(text,i)
         elsif choice == val[1]
@@ -105,9 +111,6 @@ def main()
             run = false  
             break
         end
-       
-       
-        
         i += 1
     end
 end
